a9207bbaafebb7c1ba05fdde7b78439e50e846d7
vimalKeshu <vmal.chaudhari@gmail.com>
2023-10-17 20:50:30 +0530

    Added message type and passed message content instead of message object which was causing the issue.

--- model.py
+++ model.py
@@ -76,13 +76,13 @@ async def start():
     cl.user_session.set("chain", chain)
 
 @cl.on_message
-async def main(message):
+async def main(message: cl.Message):
     chain = cl.user_session.get("chain") 
     cb = cl.AsyncLangchainCallbackHandler(
         stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
     )
     cb.answer_reached = True
-    res = await chain.acall(message, callbacks=[cb])
+    res = await chain.acall(message.content, callbacks=[cb])
     answer = res["result"]
     sources = res["source_documents"]
 
